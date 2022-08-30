0dd7395ee6341e5d91e1bf967af48eb3b49bbfee
ghoqr <112453865+ghoqr@users.noreply.github.com>
2022-08-30 15:25:09 +0900

    Update discord.py

--- discord.py
+++ discord.py
@@ -1,7 +1,7 @@
 import discord
 
 client = discord.Client()
-token = 'MTAxMzY5MTI5ODQzMzIwODM1MA.GVRfmF.9ZZmB-LgWccEKDAaza-Fkh5DfIyGHzlwPnH_vI'
+token = 'MTAxMzY5MTI5ODQzMzIwODM1MA.GEqOCg.gRdrYw0emuhZFsRB_lvmvljTkWdCzpRDgfPObw'
 
 @client.event
 async def on_ready():
